5548c413ee442331ba8a85d3b656a093e4c519e4
theliamdavidson <misterliamd@gmail.com>
2022-12-20 10:42:13 -0800

    re-did data model; other optimizations; better

--- hostR.py
+++ hostR.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    patient_instance.reset_class()
+    patient_instance.__init__()
     return render_template('home.html')
 
 @app.route('/about/')
@@ -20,37 +20,32 @@ def hello():
                             name=patient_instance.patient_name)
 
 @app.route('/update_vessel', methods=['POST','GET'])
-def change_current_vessel():
-    if patient_instance.patient_name == "":
-        patient_instance.patient_name = request.form.get("fname")
-    
-    update_vessel = request.form['vessel']
-    print(update_vessel)
-    nu_value = capture_decoder()
-    patient_instance.value_holder(nu_value)
+def change_current_vessel():  
+    patient_instance.temp_vessel_tracker = patient_instance.vessels.index(request.form['vessel'])
+    patient_instance.temp_discovered_value_holder = capture_decoder()
     return render_template("index.html", 
+                            vessels = patient_instance.vessels,
                             name=patient_instance.patient_name, 
-                            num=nu_value)
+                            num=patient_instance.temp_discovered_value_holder)
 
 @app.route('/confirm_data/', methods=['POST'])
 def confirm_data_response():
-    num = request.form.get("num")
-    patient_instance.value_holder(num, True)
-    nu_value = capture_decoder()
-    patient_instance.value_holder(nu_value)
+    patient_instance.value_holder()
+    patient_instance.temp_discovered_value_holder = capture_decoder()
     return render_template("index.html", 
-                            name=patient_instance.patient_name, 
-                            num=nu_value)
+                            vessels = patient_instance.vessels,
+                            name = patient_instance.patient_name, 
+                            num = patient_instance.temp_discovered_value_holder)
 
 @app.route('/read_data/', methods=['POST'])
 def read_data_response():
     if patient_instance.patient_name == "":
         patient_instance.patient_name = request.form.get("fname")
-    nu_value = capture_decoder()
-    patient_instance.value_holder(nu_value)
+    patient_instance.temp_discovered_value_holder = capture_decoder()
     return render_template("index.html", 
-                            name=patient_instance.patient_name, 
-                            num=nu_value)
+                            vessels = patient_instance.vessels,
+                            name = patient_instance.patient_name, 
+                            num = patient_instance.temp_discovered_value_holder)
 
 @app.route('/print_data/', )
 def print_data():
